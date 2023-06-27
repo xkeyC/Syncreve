00ec086c291386a822e4ce7bec78af988ba62eb1
xkeyC <39891083+xkeyC@users.noreply.github.com>
2023-06-27 12:28:28 +0800

    Update build_android_library.sh
    fix

--- libsyncreve/moblie/build_android_library.sh
+++ libsyncreve/moblie/build_android_library.sh
@@ -1,5 +1,5 @@
 go get golang.org/x/mobile/bind
 gomobile bind -target=android -androidapi 23 ./libsyncreve
 go mod tidy
-mv ./libsyncreve.aar ../../android\app\libs\
+mv ./libsyncreve.aar ../../android/app/libs/
 mv ./libsyncreve-sources.jar ../../android/app/libs/
